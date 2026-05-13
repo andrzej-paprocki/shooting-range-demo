af99f72e658298dc1b24eebd87e40b1ac2804e0f
andrzej-paprocki <andrzej.k.paprocki@gmail.com>
2026-05-13 23:25:48 +0200

    Delete js/target3.js

--- js/target3.js
+++
@@ -1,1 +0,0 @@
-﻿document.write('<div id="target3">');	document.write('<div class="wrapperShoots">');		document.write('<div id="cel3">cel 3:</div>');		document.write('<div class="probel"></div>');		document.write('<div id="cel3Digits"></div>');	document.write('</div>');document.write('</div>');
