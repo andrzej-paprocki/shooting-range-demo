a76fb25518142641c83e1e9b8bb3fdab337c5f3c
andrzej-paprocki <andrzej.k.paprocki@gmail.com>
2026-05-13 23:25:31 +0200

    Delete js/target5.js

--- js/target5.js
+++
@@ -1,1 +0,0 @@
-﻿document.write('<div id="target5">');	document.write('<div class="wrapperShoots">');		document.write('<div id="cel5">cel 5:</div>');		document.write('<div class="probel"></div>');		document.write('<div id="cel5Digits"></div>');	document.write('</div>');document.write('</div>');
